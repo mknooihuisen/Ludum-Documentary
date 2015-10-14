e8fe2c6f52213f7a7b8cebdec66a815a3477d76a
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-10-14 13:38:43 -0400

    added test script

---
+++ UnityTesting/Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello, Jason! (The rest of the world is not good enough for my hello.)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
