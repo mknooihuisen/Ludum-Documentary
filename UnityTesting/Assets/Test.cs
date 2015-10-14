689e2b0cf27389438b5b23f0552dd1511a6f0c25
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-10-14 13:56:29 -0400

    test

--- UnityTesting/Assets/Test.cs
+++ UnityTesting/Assets/Test.cs
@@ -6,6 +6,7 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Hello, Jason! (The rest of the world is not good enough for my hello.)");
+		Debug.Log ("This is my update");
 	}
 	
 	// Update is called once per frame
